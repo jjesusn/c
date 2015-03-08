c06b558e69a948007fdcb97b99b0ecd5d2318490
ElSuperhumano <j_100@outlook.com>
2015-03-08 14:31:52 -0600

    Hola mundo
    El primer programa de todo programador

---
+++ hola_mundo.c
@@ -0,0 +1,18 @@
+/*
+Programa: Hola_mundo
+Utilidad: Muestra en pantalla el letro "Hola Mundo"
+Programador: Jesus Navarro
+Fecha: 3 de Marzo del 2015
+Version: 1.1
+Changelog:
+-Se agregaron los datos del programa al inicio
+-Se comento el codigo
+*/
+
+#include <stdio.h> //Con esta linea se incluye el archivo para mostrar resultados en pantalla
+//Se inicia el bloque de la funcion main() con una llave {
+int main(){  //La funcion main() es el constructor principal de C
+	printf("Hola Mundo!"); //El comando printf() muestra texto en pantalla
+	return 0; //Comunica al SO que el valor es cero, es decir correcto
+} 
+//El bloque main() finaliza con el cierre de la llave }
